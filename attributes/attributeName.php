4821c610c3630eda747c5f074189d2eb20d3033f
Talant <iTalantAinu@gmail.com>
2017-01-24 10:39:08 +0100

    extended the name attribute class some, with a first name and surname, and possibility of defining either through params.

--- attributes/attributeName.php
+++ attributes/attributeName.php
@@ -2,24 +2,29 @@
 	require_once 'attributes/attributeAbstract.php';
 	
 	class attributeName extends attributeAbstract {
-		private $name;
-		private $maleNames = array('Nickolas Lepak','Winfred Slaybaugh','Myron Yankey','Sergio Thornley','Roland Newhard','Brent Kizer','Terrance Restivo','Elmer Peiffer','Kenton Fang','Reinaldo Glisson','Fredrick Thrush','Maria Denby','Lucas Constable','Bart Mcclaren','Stefan Hollow','Eldridge Hallenbeck','Rodolfo Bertram','Nathan Christner','Alonso Biron','Federico Vosburgh','Rod Vinci','Sonny Cheney','Harry Finke','Leroy Piro','Guillermo Curnutte','Erik Ouellette','Renato Valentine','Scotty Sparr','Mario Ahlquist','Micah Sommers');
-		private $femaleNames = array('Leola Transue','Mina Rodney','Bobette Becker','Larue Owen','Madlyn Jepsen','Francie Dial','Kallie Cerutti','Ester Jasmin','Nicki Scales','Leota Kilcrease','Shawnee Socia','Lashaunda Strock','Felicidad Thrush','Dominica Owsley','Madeleine Papenfuss','Glenda Sokol','Sarina Farraj','Wendi Secrest','Clementine Kendig','Dominque Closson','Vivienne Bert','Ariana Vadnais','Nu Tinsley','Tammy Parenteau','Marchelle Poitra','Lorie Hermosillo','Liza Hemenway','Shawnna Otter','Vonnie Dipalma','Danyell Grande');
+		private $firstName;
+		private $surName;
+		private $maleNames = array('Nickolas','Winfred','Myron','Sergio','Roland','Brent','Terrance','Elmer','Kenton','Reinaldo','Fredrick','Maria','Lucas','Bart','Stefan','Eldridge','Rodolfo','Nathan','Alonso','Federico','Rod','Sonny','Harry','Leroy','Guillermo','Erik','Renato','Scotty','Mario','Micah');
+		private $femaleNames = array('Leola','Mina','Bobette','Larue','Madlyn','Francie','Kallie','Ester','Nicki','Leota','Shawnee','Lashaunda','Felicidad','Dominica','Madeleine','Glenda','Sarina','Wendi','Clementine','Dominque','Vivienne','Ariana','Nu','Tammy','Marchelle','Lorie','Liza','Shawnna','Vonnie','Danyell');
+		private $surNames = array('Lepak','Slaybaugh','Yankey','Thornley','Newhard','Kizer','Restivo','Peiffer','Fang','Glisson','Thrush','Denby','Constable','McClaren','Hollow','Hallenbeck','Bertram','Christner','Biron','Vosburgh','Vinci','Cheney','Finke','Piro','Curnutte','Ouellette','Valentine','Sparr','Ahlquist','Sommers','Grande','Dipalma','Otter','Hemenway','Hermosillo','Poitra','Parenteau','Tinsley','Vadnais','Bert','Closson','Kendig','Secrest','Transue','Rodney','Becker','Owen','Jepsen','Dial','Cerutti','Jasmin','Scales','Kilcrease','Socia','Strock','Thrush','Owsley','Papenfuss','Sokol','Farraj');
 		
 		public function init($params) {
-			if (isset($params['name'])) $this->name = $params['name'];
+			if (isset($params['firstName'])) $this->firstName = $params['firstName'];
+			if (isset($params['surName'])) $this->surName = $params['surName'];
 		}
 		public function getAttributeName() {
 			return "Name";
 		}
 		public function getAttributeValue() {
-			if ($this->name == null) {
-				$namearray = ($this->character->gender == "Male") ? $this->maleNames : $this->femaleNames;
-				
-				mt_srand($this->attributeSeed);
-				$this->name = $namearray[mt_rand(0,count($namearray)-1)];
-			}
+			$namearray = ($this->character->gender == "Male") ? $this->maleNames : $this->femaleNames;
 			
-			return $this->name;
+			mt_srand($this->attributeSeed);
+			$firstnameIndex = mt_rand(0,count($namearray)-1);
+			$surnameIndex = mt_rand(0,count($this->surNames)-1);
+			
+			if ($this->firstName == null) $this->firstName = $namearray[$firstnameIndex];
+			if ($this->surName == null) $this->surName = $this->surNames[$surnameIndex];
+			
+			return $this->firstName." ".$this->surName;
 		}
 	}
